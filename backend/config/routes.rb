a44a57ecef6eb759b0d1aaab22fe6736fceb4d0b
Matt Widiger <matt.widiger@gmail.com>
2024-08-01 10:11:30 -0400

    add routes for user favorites controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   namespace :api do
+    resources :user_favorites, only: [:index, :create, :destroy]
     resources :locations, only: [:index, :show] do
       resource :weather, only: [:show]
     end
