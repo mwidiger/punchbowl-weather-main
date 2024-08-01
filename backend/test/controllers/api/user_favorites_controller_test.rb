1d2d3ec4ba38a861ff14b3b364d16eaf5b1517d4
Matt Widiger <matt.widiger@gmail.com>
2024-08-01 12:48:28 -0400

    test updated

--- backend/test/controllers/api/user_favorites_controller_test.rb
+++ backend/test/controllers/api/user_favorites_controller_test.rb
@@ -1,36 +1,40 @@
 require "test_helper"
 
 class Api::UserFavoritesControllerTest < ActionDispatch::IntegrationTest
+  user_id = Identity.current_user_id
+  location_id = Location.first.id
+  uf = UserFavorite.create(user_id: user_id, location_id: location_id)
+
   # This record should always exist because seeded
   test 'get all favorite should return json' do
     get api_user_favorites_path, as: :json
-
-    assert true
+    assert_response :success
   end
 
   test 'get all favorite should return all location_ids "favorite"d by this user' do
     get api_user_favorites_path, as: :json
-    assert true
+    assert response.parsed_body.length == 1
   end
 
-  test 'create a favorite should return the favorite if all is well' do
-    post api_user_favorites_path(location_id: 1)
-    assert true
+  test 'create a favorite should return the new favorite if all is well' do
+    new_location_id = location_id + 1
+    post api_user_favorites_path(location_id: new_location_id)
+    assert response.parsed_body['location_id'] == new_location_id
   end
 
   test 'create a favorite should throw an error if the location id is invalid' do
     post api_user_favorites_path(location_id: 999)
-    assert true
+    assert response.status == 400
   end
 
   test 'create a favorite should throw an error if the location id is not a num' do
     post api_user_favorites_path(location_id: 'foo')
-    assert true
+    assert response.status == 400
   end
 
   test 'create a favorite should throw an error if the location id is null' do
     post api_user_favorites_path(location_id: nil)
-    assert true
+    assert response.status == 400
   end
 
   test 'delete a valid favorite should reduce its count by one' do
