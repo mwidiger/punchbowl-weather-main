727f428e394118d17bdf707c04116fffeb422ebd
Matt Widiger <matt.widiger@gmail.com>
2024-08-01 12:53:39 -0400

    updated test with fix to status and TDD for delete function

--- backend/test/controllers/api/user_favorites_controller_test.rb
+++ backend/test/controllers/api/user_favorites_controller_test.rb
@@ -19,29 +19,33 @@ class Api::UserFavoritesControllerTest < ActionDispatch::IntegrationTest
   test 'create a favorite should return the new favorite if all is well' do
     new_location_id = location_id + 1
     post api_user_favorites_path(location_id: new_location_id)
+    assert_response :created
     assert response.parsed_body['location_id'] == new_location_id
   end
 
   test 'create a favorite should throw an error if the location id is invalid' do
     post api_user_favorites_path(location_id: 999)
-    assert response.status == 400
+    assert_response :bad_request
   end
 
   test 'create a favorite should throw an error if the location id is not a num' do
     post api_user_favorites_path(location_id: 'foo')
-    assert response.status == 400
+    assert_response :bad_request
   end
 
   test 'create a favorite should throw an error if the location id is null' do
     post api_user_favorites_path(location_id: nil)
-    assert response.status == 400
+    assert_response :bad_request
   end
 
   test 'delete a valid favorite should reduce its count by one' do
-    assert true 
+    delete api_user_favorites_path(location_id: location_id)
+    assert_response :no_content
+    assert UserFavorite.count == 0
   end
 
   test 'delete an invalid favorite should throw an error and count should remain the same' do
-    assert true 
+    delete api_user_favorites_path(location_id: 999)
+    assert_response :bad_request
   end
 end
