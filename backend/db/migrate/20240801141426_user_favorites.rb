584edbc527d59634ec605b6a9c0f7c9f9c68e50b
Matt Widiger <matt.widiger@gmail.com>
2024-08-02 09:35:27 -0400

    cleaned up user favorites migration

--- backend/db/migrate/20240801141426_user_favorites.rb
+++ backend/db/migrate/20240801141426_user_favorites.rb
@@ -2,11 +2,11 @@ class UserFavorites < ActiveRecord::Migration[7.0]
   def change
     create_table :user_favorites do |t|
       # TODO: Should be belongs_to but not implementing user table for this
+      # t.belongs_to :user, index: true
       t.integer :user_id
       t.belongs_to :location, index: true
 
       t.index [:user_id, :location_id], unique: true, name: 'index_favorite_user_location'
-      t.index :location, name: "index_favorite_location"
       t.timestamps
     end
   end
